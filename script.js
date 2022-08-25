ece716934cfd6f5821c14ee740e6c1330de85377
NiewiarowskiD <96947694+NiewiarowskiD@users.noreply.github.com>
2022-08-25 17:47:05 +0200

    js_tabs

--- script.js
+++ script.js
@@ -131,4 +131,68 @@ function carousel() {
     setTimeout(carousel, 10000);
 }
 
+//Oferty_TABA
 
+$(function () {
+
+    var activeIndex = $('.active-tab').index(),
+        $contentlis = $('.tabs-content li'),
+        $tabslis = $('.tabs li');
+
+    $contentlis.eq(activeIndex).show();
+
+    $('.tabs').on('click', 'li', function (e) {
+        var $current = $(e.currentTarget),
+            index = $current.index();
+
+        $tabslis.removeClass('active-tab');
+        $current.addClass('active-tab');
+        $contentlis.hide().eq(index).show();
+    });
+});
+
+
+// carousel 2 //
+$(function(){
+    var container = $(".slide_wrap");
+    var slideShow = container.find(".slide_show");
+    var slideImg = slideShow.find(".slide_img");
+    var slides = slideImg.find(">div");                //슬라이드 이미지
+    var slideBtn = container.find(".slide_btn")        //슬라이드 버튼
+
+    var slideCount = slides.length;                    //슬라이드 개수
+    var slideWidth = slides.innerWidth();                   //슬라이드 이미지의 가로 값
+    var show_num = 3;
+    var num = 0;
+
+    var slideCopy = $(".slide:lt("+show_num+")").clone();
+    slideImg.append(slideCopy);
+
+    //이미지 움직이기
+    function back(){
+        if(num == 0){
+            num == slideCount;
+            slideImg.css("margin-left",-num * slideWidth +"px" )
+        }
+        num--;
+        slideImg.animate({"margin-left": -slideWidth * num+"px"}, 500);
+    };
+    function next(){
+        if(num == slideCount){
+            num == 0;
+            slideImg.css("margin-left",-num * slideWidth +"px");
+        }
+        num++;
+        slideImg.animate({"margin-left": -slideWidth * num+"px"}, 500);
+    };
+
+    //버튼 클릭하기
+    slideBtn.on("click", "a", function(){
+        if($(this).hasClass("prev")){
+            back();
+        } else {
+            next();
+        }
+    });
+
+});
